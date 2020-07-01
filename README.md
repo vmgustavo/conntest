71ed0667dfe5095825d6497aea9614909341fa21
Gustavo Maia <vm.gustavo31@gmail.com>
2020-07-01 18:42:54 -0300

    update

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Source Material:
 * https://github.com/sivel/speedtest-cli/wiki
 * https://www.twilio.com/blog/2017/02/an-easy-way-to-read-and-write-to-a-google-spreadsheet-in-python.html
 * https://stackoverflow.com/questions/45540827/appending-pandas-data-frame-to-google-spreadsheet
+* https://towardsdatascience.com/accessing-google-spreadsheet-data-using-python-90a5bc214fd2
--- main.py
+++ main.py
@@ -1,40 +1,64 @@
-import speedtest
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import os
-import re
 from datetime import datetime
-import pytz
 
-MEGA_BYTES = pow(2, 20)
-MEGA_BITS = pow(10, 6)
+import pytz
+import gspread
+import speedtest
+from oauth2client.service_account import ServiceAccountCredentials
+
+MEGA_BYTES = 2 ** 20
+MEGA_BITS = 10 ** 6
 
 
 def main():
-    timestamp = datetime.now(tz=pytz.timezone('America/Sao_Paulo')).strftime('%Y-%m-%dT%H:%M:%S%Z')
+    # SPEEDTEST PART
+    timestamp = datetime.now(tz=pytz.timezone('America/Sao_Paulo')).isoformat()
     print('SERVICE START AT ' + timestamp)
-    file_path = re.match(r'(/.+/)', os.path.abspath(__file__)).group(0)
 
-    s = speedtest.Speedtest()
+    s = speedtest.Speedtest(
+        config={
+            # TODO: Customize to transfer less data
+            'sizes': {
+                'upload': [524_288, 1_048_576, 3_145_728],
+                'download': [350, 500, 750, 1_000, 1_500, 2_000, 2_500, 3_000, 3_500, 4_000]
+            },
+            'counts': {'upload': 17, 'download': 4},
+            'threads': {'upload': 2, 'download': 8},
+            'length': {'upload': 10, 'download': 10},
+            'upload_max': 51
+        }
+    )
+
+    st = datetime.now()
     s.get_best_server()
     s.download()
     s.upload()
 
-    # ['timestamp', 'download', 'upload', 'ping', 'latency', 'sponsor', 'id', 'host']
-    data = [timestamp,
-            (s.results.download/MEGA_BITS), (s.results.upload/MEGA_BITS),
-            (s.results.bytes_sent/MEGA_BYTES), (s.results.bytes_received/MEGA_BYTES),
-            s.results.ping,
-            s.results.server['sponsor'], s.results.server['name'],
-            s.results.server['country'], s.results.server['id']]
+    exec_time = (datetime.now() - st).seconds
 
+    res = s.results.dict()
+
+    data = {
+        'exec_time': exec_time,
+        'date': timestamp,
+        'ping': res['ping'],
+        'download': res['download'] / MEGA_BITS,
+        'bytes_received': res['bytes_received'] / MEGA_BYTES,
+        'upload': res['upload'] / MEGA_BITS,
+        'bytes_sent': res['bytes_sent'] / MEGA_BYTES,
+        'server_loc': ' '.join([res['server']['name'], res['server']['cc']]),
+        'server_host': res['server']['host'],
+        'server_latency': res['server']['latency'],
+        'client_ip': res['client']['ip']
+    }
+
+    # GOOGLE DRIVE API PART
     scope = ['https://spreadsheets.google.com/feeds',
              'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name((file_path + 'conntest-cred.json'), scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name('conntest-cred.json', scope)
     client = gspread.authorize(creds)
 
     sheet = client.open('conntest_results').sheet1
-    sheet.append_row(values=data)
+    sheet.append_row(values=list(data.values()))
 
 
 if __name__ == "__main__":
